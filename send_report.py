417d427a65128f28e9caaadaa397e313940c9ba5
ahmedsaudai33-commits <ahmedsaud.ai.33@gmail.com>
2026-03-27 16:14:30 +0000

    send_report.py

--- send_report.py
+++ send_report.py
@@ -4,7 +4,21 @@ import requests
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-message = "🔥 تقرير اليوم جاهز"
+message = """
+🔥 فكرة اليوم
+
+🎯 العنوان:
+كيف تجيب مشاهدات عالية؟
+
+🧠 الهوك:
+في حركة بسيطة تخلي الفيديو ينفجر
+
+📹 الفكرة:
+ابدأ بمشهد قوي + سؤال يخلّي الناس تكمل
+
+📊 الهدف:
+رفع المشاهدات والمتابعين
+"""
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
